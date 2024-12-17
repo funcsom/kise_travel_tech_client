e7976ada72884a11acfd1a8fe7ca7b5edeeea560
funcsom <bishop@kau.kr>
2024-12-17 09:07:26 +0900

    최상단 App에서 React Router 사용, 세팅 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import Header from "./components/Header";
-import Section from "./components/Section";
-import styles from "./App.module.css";
+import { Routes, Route } from "react-router-dom";
+import LovingLocal from "./pages/loving-local-page/LovingLocal";
 
 function App() {
-  const handleclick = (e) => {
-    console.log(e);
-  };
   return (
-    <div className={styles.App}>
-      <Header
-        handleClickLeft={handleclick}
-        text="지역사랑여행"
-        handleClickRight={handleclick}
-      />
-      <main>
-        <Section title="추천 이벤트" subtitle="코레일에서 추천하는 이벤트" />
-        <Section
-          title="지역별 여행 상품"
-          subtitle="지역별 대표 패키지 여행 상품"
-        />
-      </main>
-    </div>
+    <Routes>
+      <Route path="/" element={<LovingLocal />} />
+    </Routes>
   );
 }
 
