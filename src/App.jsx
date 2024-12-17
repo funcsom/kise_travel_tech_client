97d7d8eae3e0a1dc1883153ab353f57c6827a2f4
funcsom <bishop@kau.kr>
2024-12-17 13:10:58 +0900

    App 컴포넌트에 local-trip 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LovingLocal from "./pages/loving-local-page/LovingLocal";
+import LocalTrip from "./pages/local-trip-page/LocalTrip";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LovingLocal />} />
+      <Route path="/localtrip" element={<LocalTrip />} />
     </Routes>
   );
 }
