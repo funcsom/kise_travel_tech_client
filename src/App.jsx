056a854f7e97aeabf52ef707292e0309957cbf9b
funcsom <bishop@kau.kr>
2024-12-20 10:02:17 +0900

    라우팅 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import LovingLocal from "./pages/loving-local-page/LovingLocal";
-import LocalTrip from "./pages/local-trip-page/LocalTrip";
+import LovingLocal from "./pages/1-loving-local-page/LovingLocal";
+import LocalTrip from "./pages/2-local-trip-page/LocalTrip";
 import Component from "./components/common/ComponentExample";
-import SelectProduct from "./pages/select-product-page/SelectProduct";
-import SelectDetail from "./pages/select-detail-page/SelectDetail";
-import TravelerAgree from "./pages/traveler-agree-page/TravelerAgree";
+import SelectProduct from "./pages/4-select-product-page/SelectProduct";
+import SelectDetail from "./pages/5-select-detail-page/SelectDetail";
+import TravelerAgree from "./pages/6-traveler-agree-page/TravelerAgree";
 import styles from "./App.module.css";
 
 function App() {
