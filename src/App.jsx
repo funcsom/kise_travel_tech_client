06600cccbcb27b2dc1f039e2fb2e2b593be12448
funcsom <bishop@kau.kr>
2024-12-20 09:39:16 +0900

    여행자 약관동의 페이지 라우팅 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import LocalTrip from "./pages/local-trip-page/LocalTrip";
 import Component from "./components/common/ComponentExample";
 import SelectProduct from "./pages/select-product-page/SelectProduct";
 import SelectDetail from "./pages/select-detail-page/SelectDetail";
+import TravelerAgree from "./pages/traveler-agree-page/TravelerAgree";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LovingLocal />} />
-      <Route path="/localtrip" element={<LocalTrip />} />
-      <Route path="/selectproduct" element={<SelectProduct />} />
-      <Route path="/selectdetail" element={<SelectDetail />} />
-    </Routes>
+    <div className={styles.App}>
+      <Routes>
+        <Route path="/" element={<LovingLocal />} />
+        <Route path="/localtrip" element={<LocalTrip />} />
+        <Route path="/selectproduct" element={<SelectProduct />} />
+        <Route path="/selectdetail" element={<SelectDetail />} />
+        <Route path="/traveleragree" element={<TravelerAgree />} />
+      </Routes>
+    </div>
   );
 }
 
