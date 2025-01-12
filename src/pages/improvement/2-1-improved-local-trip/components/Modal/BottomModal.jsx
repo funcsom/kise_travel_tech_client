be21b865b2b8e7a08fd7214c26702e573facb754
funcsom <bishop@kau.kr>
2025-01-12 20:21:48 +0900

    개선화면 마지막 커밋

--- src/pages/improvement/2-1-improved-local-trip/components/Modal/BottomModal.jsx
+++ src/pages/improvement/2-1-improved-local-trip/components/Modal/BottomModal.jsx
@@ -11,6 +11,41 @@ import resetbutton from "../../../../../assets/icon/improved/reset_button.svg";
 
 const serviceKey = import.meta.env.VITE_ENCODING;
 
+const stationlist = [
+  "청량리",
+  "상봉",
+  "양평",
+  "만종",
+  "횡성",
+  "평창",
+  "진부",
+  "정동진",
+  "묵호",
+  "동해",
+  "덕소",
+  "용문",
+  "지평",
+  "석불",
+  "일신",
+  "매곡",
+  "양동",
+  "삼산",
+  "서원주",
+  "원주",
+  "제천",
+  "영월",
+  "예미",
+  "민둥산",
+  "사북",
+  "고한",
+  "태백",
+  "동백산",
+  "도계",
+  "신기",
+];
+
+const neededLocal = ["서울특별시", "경기도", "강원도", "충청북도"];
+
 const BottomModal = ({
   selectedStation,
   setSelectedStation,
@@ -28,6 +63,11 @@ const BottomModal = ({
     };
   }, []);
 
+  useEffect(() => {
+    console.log(stationList);
+  }, [stationList]);
+
+  // 상의 이후 확정할 코드
   // 도시 코드 목록 가져오기
   useEffect(() => {
     fetch(
@@ -36,7 +76,14 @@ const BottomModal = ({
       .then((response) => response.json())
       .then((data) => {
         const cityList = data.response.body.items.item;
-        setCityCodeList(cityList);
+
+        // 아래의 setCityCodeList(cityList); 주석을 해제시키고 filteredList에 관해서 지우면 모든 로컬을 확인할 수 있음
+        const filteredList = cityList.filter((node) =>
+          neededLocal.includes(node.cityname)
+        );
+
+        // setCityCodeList(cityList);
+        setCityCodeList(filteredList);
       })
       .catch((error) => {
         console.error("Error fetching city code list:", error);
@@ -51,7 +98,14 @@ const BottomModal = ({
       .then((response) => response.json())
       .then((data) => {
         const nodelist = data.response.body.items.item;
-        setStationList(nodelist);
+
+        // 아래의 setStationList(nodelist); 주석을 해제시키고 filteredList에 관해서 지우면 모든 출발역을 확인할 수 있음
+        const filteredList = nodelist.filter((node) =>
+          stationlist.includes(node.nodename)
+        );
+
+        // setStationList(nodelist);
+        setStationList(filteredList);
       })
       .catch((error) => {
         console.error("Error fetching station list:", error);
@@ -64,7 +118,7 @@ const BottomModal = ({
 
   const addStation = (node) => {
     // 기존에 존재하는 node이면 추가 안하도록 처리
-    if (!selectedStation.some((station) => station === `${node}역`)) {
+    if (!selectedStation.some((station) => station === node)) {
       setSelectedStation([...selectedStation, node]);
     }
   };
